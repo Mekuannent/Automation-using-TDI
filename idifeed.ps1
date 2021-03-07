c6ad550528cc7b16bbc0b09e50bbb0be8e7b9db0
Meku <selamta@gmail.com>
2021-03-07 15:17:45 -0500

    Update idifeed.ps1

--- idifeed.ps1
+++ idifeed.ps1
@@ -8,13 +8,14 @@ if($args.count -ne 2){
 #Eg if Directory solution is tdisol-dev, Dev signifies env
 $currentDir=(get-location).path
 $currentEnv=split-path(get-location).path -leaf
-if($currentEnv -eq "tdifeed"){
+if($currentEnv -eq "idifeed"){
  $scriptName=$MyInvocation.MyCommand.Name
  write-output "$scriptName cannot be invoked from $currentDir"
  write-output "Exiting with code rc=2"
  exit 2
 }
 F:\scripts\tdi-idifeed $args[0] $args[1] toolsol-$currrentEnv
+#Directory maybe toolsol-dev, toolsol-prod etc.
 #Archive files under ("*.completed") and rename
 Move-Item $currentDir\data\*.completed $currentDir\archives
 $date=Get-Date -format "yyyy.MMM.dd.hh.mm.ss"
